d65e6c897c82bc31c465f86833fb496d69fec75d
Narasimha Badrinath <bnarasimha21@gmail.com>
2017-05-27 20:24:45 +0530

    CodeSnipApi wihich Retrieves and Adds code snippets Initial Commit

---
+++ codesnipapi/server.js
@@ -0,0 +1,52 @@
+// Setup
+
+var express = require('express');
+var app = express();
+var morgan = require('morgan');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+mongoose.connect('mongodb://bnarasimha21:1nvin$ible@cluster0-shard-00-00-shwiy.mongodb.net:27017/CodeSnippets?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin');
+
+// Configuration
+app.use(express.static(__dirname + '/public'));
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.json());
+app.use(bodyParser.json({type: 'application/vhd.api+json'}));
+app.use(methodOverride());
+
+// Model
+var CodeSnippet = mongoose.model('CodeSnippet', {
+    codesnippet : String,
+    language : String,
+    title : String
+});
+
+app.get('/api/codeSnippets', function(req, res){
+    CodeSnippet.find({}, function(err, codeSnippets){
+        if(err){
+            console.log(err);
+            res.send(err);
+        }
+        console.log(codeSnippets); 
+        res.json(codeSnippets);
+    });
+});
+
+app.post('/api/addCodeSnippet', function(req, res){
+    var newCodeSnippet = new CodeSnippet(
+        {
+            language:req.body.language,
+            title: req.body.title,
+            codesnippet: req.body.codesnippet
+        });    
+    CodeSnippet.create(newCodeSnippet, function(err, codeSnippets){
+        if(err) return handleError(err);
+    });
+});
+
+//Run
+app.listen('8082');
+console.log('App is listening on 8082 port');
