ad6fc3fd65b48b3b772f8e363f2148d0ddf0f90b
Narasimha Badrinath <bnarasimha21@gmail.com>
2017-07-03 15:51:42 +0530

    ignore dockerfile when packaging

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var zip = require('gulp-zip');
 
 gulp.task('deploy', function(){
-    return gulp.src(['**', '!package.json', '!gulpfile.js'])
+    return gulp.src(['**', '!package.json', '!gulpfile.js', '!Dockerfile'])
     .pipe(zip('deploy.zip'))
     .pipe(gulp.dest('dest/'))
 });
