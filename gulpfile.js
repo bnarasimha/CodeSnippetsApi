647bc4b21b54dced188baff2f2e0578f6caa9d83
Narasimha Badrinath <bnarasimha21@gmail.com>
2017-07-05 15:38:38 +0530

    exclude node_modules properly

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var zip = require('gulp-zip');
 
 gulp.task('deploy', function(){
-    return gulp.src(['**', '!gulpfile.js', '!Dockerfile', 'node_modules/**'])
+    return gulp.src(['**', '!gulpfile.js', '!Dockerfile', '!node_modules/**'])
     .pipe(zip('deploy.zip'))
     .pipe(gulp.dest('dest/'))
 });
