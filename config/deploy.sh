174422e3360afd0ac264d7c241b58e6de7f201ba
Narasimha Badrinath <bnarasimha21@gmail.com>
2017-07-11 11:15:40 +0530

    changed scriipt to accept environment variable as parameter

--- config/deploy.sh
+++ config/deploy.sh
@@ -1,7 +1,8 @@
-export NODE_ENV=prod; 
+export NODE_ENV=$1; 
 cd api; 
 sudo rm -r node_modules; 
 sudo unzip -o ~/artifacts/api/deploy.zip; 
 sudo chmod 777 node_modules; 
+sudo chmod 777 config;
 sudo npm install; 
 sudo pm2 restart api;
