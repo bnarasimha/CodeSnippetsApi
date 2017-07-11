7d286114d6a042eb7f137fd5a1727c608c032b06
Narasimha Badrinath <bnarasimha21@gmail.com>
2017-07-11 13:16:44 +0530

    updated mongoose, changed config test, added log for debugging

--- config/config.test.js
+++ config/config.test.js
@@ -1,5 +1,5 @@
 var config = require('./config.local')
-config.env = 'prod'
+config.env = 'test'
 config.MongoUri = 'mongodb://bnarasimha21:1nvin$ible@cluster0-shard-00-00-shwiy.mongodb.net:27017/CodeSnippetsTest?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin'
 
 module.exports = config;
--- config/index.js
+++ config/index.js
@@ -1,3 +1,5 @@
 var env = process.env.NODE_ENV || 'local'
+console.log('process.env.NODE_ENV: ' + process.env.NODE_ENV)
+
 var cfg = require('./config.' + env)
 module.exports = cfg;
