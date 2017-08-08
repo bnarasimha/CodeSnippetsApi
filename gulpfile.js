8964689abd6467e7dbf48b9f7125c0d732c0d25d
Narasimha Badrinath <bnarasimha21@gmail.com>
2017-08-08 16:55:13 +0530

    zip step removed in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,10 @@
 var gulp = require('gulp');
-var zip = require('gulp-zip');
 
 gulp.task('deploy', function(){
     return gulp.src(['**', '!gulpfile.js', '!Dockerfile', '!node_modules/**'])
-    .pipe(zip('deploy.zip'))
     .pipe(gulp.dest('dest/'))
 });
 
-gulp.task('deploysh', function(){
-    return gulp.src(['config/deploy.sh'])
-    .pipe(gulp.dest('dest/'))
-})
-
-gulp.task('default', ['deploysh', 'deploy'], function(){
+gulp.task('default', ['deploy'], function(){
     
 })
