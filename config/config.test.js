3e79de628ec6268951170650dab4298920b5db23
Narasimha Badrinath <bnarasimha21@gmail.com>
2018-11-24 09:25:31 +0530

    changed port number to 8081

--- config/config.test.js
+++ config/config.test.js
@@ -1,6 +1,6 @@
 var config = require('./config.local')
 config.env = 'test'
-config.port = 8091
+config.port = 8081
 config.MongoUri = 'mongodb://bnarasimha21:1nvin$ible@cluster0-shard-00-00-shwiy.mongodb.net:27017/CodeSnippetsTest?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin'
 
 module.exports = config;
