656c34848925224b31f46d30d34da2508366ae95
Narasimha Badrinath <bnarasimha21@gmail.com>
2018-12-15 00:44:37 +0530

    Since deploying to Azure App Service changed port numbers to 80

--- config/config.test.js
+++ config/config.test.js
@@ -1,6 +1,6 @@
 var config = require('./config.local')
 config.env = 'test'
-config.port = 8081
+config.port = 80
 config.MongoUri = 'mongodb://bnarasimha21:1nvin$ible@cluster0-shard-00-00-shwiy.mongodb.net:27017/CodeSnippetsTest?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin'
 
 module.exports = config;
