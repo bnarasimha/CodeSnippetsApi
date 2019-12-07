1bdc91f6290f841592c4ab1e257a1b76102aea9e
Narasimha Badrinath <bnarasimha21@gmail.com>
2019-12-07 17:41:57 +0530

    Get unique tags from DB to show on UI

--- index.js
+++ index.js
@@ -234,7 +234,33 @@ app.delete('/api/deleteCodeSnippet/:codeSnipId', function(req, res){
     });
 }); 
 
-
+// Get popular tags
+app.get('/api/getTags', function(req, res){
+    CodeSnippet.find({}, 'tags', function(err, codeSnippet){
+        var tagsList = [];
+        var length = codeSnippet.length;
+        for(i =0; i< length; i++){
+            if(codeSnippet[i].tags != null){
+                
+                var p = codeSnippet[i].tags.split(',')
+                if(p.length > 0)
+                {
+                    for(j=0; j< p.length; j ++){
+                        tagsList.push(p[j]);
+                    }
+                }
+                else{
+                    tagsList.push(p);
+                }
+            }
+        }
+        // Get unique tags
+        var tags = tagsList.filter(function(tag, i, tagsList) {
+            return tagsList.indexOf(tag) == i;
+        });
+        res.json(tags);
+    });
+});
  
 //Run
 //var port = cfg.port;
