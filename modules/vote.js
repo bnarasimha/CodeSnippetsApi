3839eb1e81ec79cb3b746a2fcffefce1bd028247
Narasimha Badrinath <bnarasimha21@gmail.com>
2020-01-10 21:47:39 +0530

    renamed api

--- modules/vote.js
+++ modules/vote.js
@@ -3,7 +3,7 @@ var setup = require('../setup.js');
 var app = setup.app;
 var Vote = setup.Vote;
 
-app.get('/api/getVotes/:codeSnipId', function(req, res){
+app.get('/api/getCodeSnippetVotes/:codeSnipId', function(req, res){
     Vote.find({codeSnippetId : req.params.codeSnipId})
     .exec(function(err, Vote){
         if(err){
